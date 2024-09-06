69e41ffc0944fefd4ca86ba33045b59728afccfe
Alisson Henrique <alisson-henri@live.com>
2024-09-06 14:20:35 -0300

    aula 9

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,23 +1,101 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ProductSearchComponent } from './product-search.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HttpTestingController  } from '@angular/common/http/testing';
-describe('ProductSearchComponent', () => {
-  let component: ProductSearchComponent;
-  let fixture: ComponentFixture<ProductSearchComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProductSearchComponent,NoopAnimationsModule, HttpTestingController ],
-    }).compileComponents();
+import { mockProducts, ProductSearchService } from '@ecommerce/product-data-access';
+import { of } from 'rxjs';
+ 
 
-    fixture = TestBed.createComponent(ProductSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  describe('ProductSearchComponent', () => {
+    let component: ProductSearchComponent;
+    let fixture: ComponentFixture<ProductSearchComponent>;
+    let productSearchService: ProductSearchService;
+  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [ProductSearchComponent, NoopAnimationsModule],
+        providers: [
+          {
+            provide: ProductSearchService, 
+            useValue: { searchByName: () => of(mockProducts) }, 
+          },
+        ],
+      }).compileComponents();
+  
+      productSearchService = TestBed.inject(ProductSearchService);
+      fixture = TestBed.createComponent(ProductSearchComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should debounce when input field is changed',fakeAsync(() => {
+      jest.spyOn(productSearchService, 'searchByName');
+      const input: HTMLInputElement =  fixture.nativeElement.querySelector('input');
+      input.value = 'tv';
+      input.dispatchEvent(new Event('input'));
+      expect(productSearchService.searchByName).not.toHaveBeenCalled();
+
+      tick(500);
+  
+      expect(productSearchService.searchByName).toHaveBeenCalledWith(input.value);
+
+    }))
+
+    it('should search multiple times',fakeAsync( () => {
+      jest.spyOn(productSearchService, 'searchByName');
+          const input: HTMLInputElement =
+            fixture.nativeElement.querySelector('input');
+
+          input.value = 'tv';
+          input.dispatchEvent(new Event('input'));
+
+          tick(500);
+
+          input.value = 'notebook';
+          input.dispatchEvent(new Event('input'));
+          tick(500);
+
+        expect(productSearchService.searchByName).toHaveBeenCalledTimes(2);
+
+    }))
+    it('should prevent identical submissions', fakeAsync(() => {
+      jest.spyOn(productSearchService, 'searchByName');
+      const input: HTMLInputElement =
+        fixture.nativeElement.querySelector('input');
+  
+      input.value = 'tv';
+      input.dispatchEvent(new Event('input'));
+  
+      tick(500);
+  
+      input.value = 'tv';
+      input.dispatchEvent(new Event('input'));
+  
+      tick(500);
+  
+      expect(productSearchService.searchByName).toHaveBeenCalledTimes(1);
+    }));
+    it('should prevent empty submissions', fakeAsync(() => {
+      jest.spyOn(productSearchService, 'searchByName');
+      const input: HTMLInputElement =
+        fixture.nativeElement.querySelector('input');
+  
+      input.value = '';
+      input.dispatchEvent(new Event('input'));
+  
+      tick(500);
+  
+      expect(productSearchService.searchByName).not.toHaveBeenCalled();
+    }));
+  
+    it('should return products observable correctly', () => {
+      component.products$.subscribe((products) => {
+        expect(products).toEqual(mockProducts);
+      });
+    });
 });
