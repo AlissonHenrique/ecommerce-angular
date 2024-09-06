f3198a95e54af985206e33b9eba6091ff6de8f7c
Alisson Henrique <alisson-henri@live.com>
2024-09-06 12:47:15 -0300

    aula 8

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductSearchComponent } from './product-search.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpTestingController  } from '@angular/common/http/testing';
 describe('ProductSearchComponent', () => {
   let component: ProductSearchComponent;
   let fixture: ComponentFixture<ProductSearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductSearchComponent,NoopAnimationsModule],
+      imports: [ProductSearchComponent,NoopAnimationsModule, HttpTestingController ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductSearchComponent);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-
+import { HttpTestingController  } from '@angular/common/http/testing';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, NxWelcomeComponent, HttpTestingController, RouterModule.forRoot([])],
     }).compileComponents();
   });
 
