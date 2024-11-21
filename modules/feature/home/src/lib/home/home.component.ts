65849ed6d0920ef2a7aa4f8c38ee45e2044970c2
Alisson Henrique <alisson-henrique@live.com>
2024-11-21 09:08:35 -0300

    add

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,18 +1,22 @@
+ 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
-import { mockProducts } from '@ecommerce/product-data-access';
+ 
+import {  RecommendedProductsService } from '@ecommerce/product-data-access';
+import { ProductCardComponent } from '@ecommerce/product-ui';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'lib-home',
   standalone: true,
-  imports: [CommonModule,MatCardModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  products$:Observable<any>
 
-
-  products = mockProducts
-
+  constructor(private recommendedProductsService: RecommendedProductsService) {
+    this.products$ = this.recommendedProductsService.getProducts();
+  }
 
 }
---
+++ modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '@ecommerce/product-data-access';
+
+@Component({
+  selector: 'lib-product-card',
+  standalone: true,
+  imports: [CommonModule,MatCardModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+
+}
